0345f2f861482e28ecfaa29ea066055104a2354a
unknown <axmedxabiib2018@gmail.com>
2020-06-09 19:23:28 +0300

    modified files

--- js/app.js
+++ js/app.js
@@ -7,22 +7,27 @@ function animatedForm() {
             const parent = arrows.parentElement;
             const nextForm = parent.nextElementSibling;
 
-            // Todo check for validation user
+            //  check for validation 
             if (input.type === "text" && validateUser(input)) {
                 nextSlide(parent, nextForm);
+            } else if (input.type === 'email' && validateEmail(input)) {
+                nextSlide(parent, nextForm)
+            } else if (input.type === "password" && validateUser(input)) {
+                nextSlide(parent, nextForm);
+            } else {
+                parent.style.animation = "shake 0.5s ease"
             }
 
-            // Todo check for validation email
-            if (input.type === 'email' && validateEmail(input)) {
-                nextSlide(parent, nextForm)
-            }
+            // get rid of animation
+            parent.addEventListener("animationend", () => {
+                parent.style.animation = "";
+            })
         });
     });
 }
 
 function validateUser(user) {
     if (user.value.length < 6) {
-        console.log('not enough charecters');
         error('rgb(189,87,87)');
     } else {
         error('rgb(87, 189, 130)');
@@ -43,7 +48,6 @@ function validateEmail(email) {
 function nextSlide(parent, nextForm) {
     parent.classList.add('innactive');
     parent.classList.remove("active");
-    console.log(nextForm.classList.add("active"))
     nextForm.classList.add("active")
 }
 
